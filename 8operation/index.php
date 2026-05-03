bfaba0339f8d5f56656f5e0fc68c58b3e059ff73
Shubham Tidke <tidkeshubham826@gmail.com>
2026-05-03 18:47:58 +0530

    8

---
+++ 8operation/index.php
@@ -0,0 +1,41 @@
+<!-- insert data in database -->
+<!-- <?php 
+    // include("config/database.php");
+    // $date=date("Y-m-d H:i:s");
+    // $sql="insert into users (username,password,created_at)
+    //         values ('mohan','123','$date')";
+    // $result=$conn->query($sql);
+    // if($result){
+    //     echo "inserted data into table <br>";
+    // }else{
+    //     echo "have some error ".$conn->error;
+    // }
+
+?> -->
+
+
+
+<!-- DISPLAY DATA -->
+<?php 
+    // include("config/database.php");
+    
+    // $sql="select * from users";
+
+    // $result=$conn->query($sql);
+    // //print_r($result);
+    // if($result->num_rows>0){
+    //     while($row=$result->fetch_assoc()){
+    //         echo "<br>";
+    //         print_r($row);
+    //     }
+    // }else{
+    //     echo "NO DATA";
+    // }
+
+?> 
+
+
+
+<!-- UPDATE User -->
+
+
